c9cac3ba0fc35b6a28631ed246fbb88d2433dd9e
AxelOP96 <axeelleguero@gmail.com>
2021-06-06 01:35:55 -0300

    Update js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-async function fetchData(e) {
+/*async function fetchData(e) {
     if (e.key == "Enter" && e.target.value != "") { 
-    const inputValue = document.getElementById("user").value;
-    const data = await fetch(`https://pokeapi.co/v2/pokemon/${inputValue}`);
-    const json = await data.json();
+    var inputValue = document.getElementById("user").value;
+    var data = await fetch(`https://pokeapi.co/v2/pokemon/${inputValue}`);
+    var json = await data.json();
 
     document.getElementById('pokemonName').innerText = json.name;
     document.getElementById('type').innerText = json.type;
@@ -12,6 +12,24 @@ async function fetchData(e) {
     // console.log(data);
     }
 }
-const input = document.getElementById("pokemonId")
+var input = document.getElementById("pokemonId");
 
-input.addEventListener("keypress", e => fetchData(e));
+input.addEventListener("keypress", e  fetchData(e));*/
+var buton = document.getElementById('buton');
+
+buton.addEventListener('click', () => {
+    var image = document.getElementById('img');
+    var p = document.getElementById('info');
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "https://pokeapi.co/v2/pokemon/3");
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status ==200)
+        {
+            var datoPokemon = JSON.parse( this.responseText);
+            img.setAttribute("src", datoPokemon.sprites.front_default);
+            
+        }
+    };
+} );
